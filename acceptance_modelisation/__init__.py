efa8e74e9044d136b9316dea4d15f0991490e664
Marie-Sophie Carrasco <carrasco@cppm.in2p3.fr>
2024-11-12 16:18:03 +0000

    fixed acceptance_modelisation init issue

--- acceptance_modelisation/__init__.py
+++ acceptance_modelisation/__init__.py
@@ -1,4 +1,3 @@
-from .base_radial_acceptance_map_creator import BaseRadialAcceptanceMapCreator
 from .base_acceptance_map_creator import BaseAcceptanceMapCreator
 from .grid3d_acceptance_map_creator import Grid3DAcceptanceMapCreator
 from .radial_acceptance_map_creator import RadialAcceptanceMapCreator
