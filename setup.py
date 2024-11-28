6db2ee6f5d83943f4680803693249bf9739d7e7c
Marie-Sophie Carrasco <carrasco@cppm.in2p3.fr>
2024-11-28 15:04:39 +0000

    removed gammapy version requirement

--- setup.py
+++ setup.py
@@ -14,7 +14,6 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     install_requires=[
-        'gammapy>=1.1,<1.2',
         'numpy',
         'scipy',
         'astropy>=4.0',
