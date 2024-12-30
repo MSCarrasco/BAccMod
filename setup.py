26671c68749bf2b4a30de88d99b35245ef5de73f
MSCarrasco <123650191+MSCarrasco@users.noreply.github.com>
2024-12-30 09:49:45 +0100

    Remove gammapy requirement from setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,6 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     install_requires=[
-        'gammapy>=1.1,<1.4',
         'numpy',
         'scipy',
         'astropy>=4.0',
